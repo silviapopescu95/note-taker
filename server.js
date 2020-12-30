b0679c9a71e3fbd40776eb0486b1e253232ec073
Silvia Popescu <amazingaivlis@gmail.com>
2020-12-30 14:55:34 -0800

    Created server.js file, creating an Express app, port, middleware, and starting the server.

---
+++ server.js
@@ -0,0 +1,24 @@
+// import express
+var express = require("express");
+
+// add routes as dependencies
+var apiRoutes = require("./routes/apiRoutes");
+var htmlRoutes = require("./routes/htmlRoutes");
+
+// initialize Express app 
+var app = express();
+
+// Create a port for server
+var PORT = process.env.PORT || 3300;
+
+// Middleware for Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Middleware to serve our static files
+app.use(express.static("public"));
+
+
+// Start server
+app.listen(PORT, function() {
+    console.log('Listening on PORT: ' + PORT);
+});
