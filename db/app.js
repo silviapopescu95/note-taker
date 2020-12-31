252574cf6b9dff1be1cce4262b61adba2c2f34aa
Silvia Popescu <amazingaivlis@gmail.com>
2020-12-31 08:05:18 -0800

    Imported uuid, created Class for NotesApp.

--- db/app.js
+++ db/app.js
@@ -6,3 +6,31 @@ var fs = require("fs");
 // for reading and writing to files
 var readFileAysnc = util.promisify(fs.readFile);
 var writeFileAysnc = util.promisify(fs.writeFile);
+
+// creates unique IDs for each note
+const {v1 : uuidv1} = require('uuid');
+
+class NotesApp {
+    read() {
+        //function to read notes
+    }
+
+    write() {
+        //function to write notes
+    }
+
+    getNotes() {
+        //function to get all notes
+    }
+
+    addNote() {
+        //function to add a new note
+    }
+
+    deleteNote() {
+        //function to delete a note by uuid created
+    }
+}
+
+//exports NotesApp to be used by apiRoutes.js
+module.exports = new NotesApp();
